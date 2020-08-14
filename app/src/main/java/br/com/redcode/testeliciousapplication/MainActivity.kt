031043620a98c318580dce743b93a598066ed255
pedrofsn <pedrokra@gmail.com>
2020-08-14 16:39:06 -0300

    Button action

--- app/src/main/java/br/com/redcode/testeliciousapplication/MainActivity.kt
+++ app/src/main/java/br/com/redcode/testeliciousapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package br.com.redcode.testeliciousapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            val input = getInputText()
+            toast(input)
+        }
     }
+
+    private fun toast(input: String) {
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getInputText() = editText.text?.trim().toString()
+
 }
